336ba160bc8e3863777a76f040f8f6454a172a0d
yelizesgi <yelizesgi2014@gmail.com>
2023-07-12 19:19:04 +0300

    project devam

---
+++ React_project/objektifcocuktestleri-app/src/helpers/data.js
@@ -0,0 +1,91 @@
+export const data = [
+    {
+      name: "AGTE",
+      img: "https://cdn.pixabay.com/photo/2017/12/13/16/01/brain-3017071_640.png",
+      statistics: [
+        "📝Bilişsel Gelişim",
+        "💕Duygusal Gelişim",
+        "💃🕺Sosyal Gelişim",
+        
+      ],
+    },
+    {
+      name: "Bender Gestalt",
+      img: "https://cdn.pixabay.com/photo/2018/11/20/08/47/children-3826830_640.jpg",
+      statistics: [
+        "🦻Alıcı Dil",
+        "🗣️İfade Edici Dil",
+        "🎯Odaklanma",
+        
+      ],
+    },
+    {
+      name: "Frankfurter-Konsantrasyon",
+      img: "https://cdn.pixabay.com/photo/2018/02/05/10/03/human-3131802_640.jpg",
+      statistics: [
+        "🎯Odaklanma",
+        "🔍Konsantrasyon",
+        "📉Strateji",
+        "🤌Sabır",
+      ],
+    },
+    {
+      name: "Goodenough",
+      img: "https://cdn.pixabay.com/photo/2023/05/13/13/09/street-art-7990475_640.jpg",
+      statistics: [
+        "💃Kadın Resmi",
+        "🕺Erkek Resmi",
+        "📝Ayrıntılı Puanlama",
+        
+      ],
+    },
+    {
+      name: "Gesell Gelişim Figürleri",
+      img: "https://cdn.pixabay.com/photo/2013/08/06/22/40/geometric-solids-170377_640.jpg",
+      statistics: [
+        "🏃‍♀️Motor Beceri",
+        "🎭Kopya Etme",
+        "🔤İnce Motor Beceri",
+        "🤸‍♀️Kaba Motor Beceri",
+      ],
+    },
+    {
+      name: "Kent Porteus",
+      img: "https://cdn.pixabay.com/photo/2017/05/13/09/04/question-mark-2309040_640.jpg",
+      statistics: [
+        "🦻Alıcı Dil",
+        "🗣️İfade Edici Dil",
+        "📝Bilişsel Gelişim",
+        "🎯Odaklanma",
+      ],
+    },
+    {
+      name: "Metropolitan(Okul Olgunluğu Testi)",
+      img: "https://cdn.artphotolimited.com/images/60913d60bd40b85323893a87/1000x1000/hakeem-olajuwon-von-den-houston-rockets.jpg",
+      statistics: [
+        "🦻Alıcı Dil",
+        "🗣️İfade Edici Dil",
+        "📝Bilişsel Gelişim",
+        "🎯Odaklanma",
+      ],
+    },
+    {
+      name: "Shaquille O'Neal",
+      img: "https://staticg.sportskeeda.com/editor/2018/10/6bb16-15398694878129-800.jpg",
+      statistics: [
+        "28,596 points",
+        "13,099 rebaunds",
+        "3,026 assist",
+        "15 All Star",
+      ],
+    },
+    {
+      name: "Scottie Pippen",
+      img: "https://basketballforever.com/wp-content/uploads/2020/04/Scottie-Pippen-Chicago-Bulls.jpg",
+      statistics: [
+        "18,940 points",
+        "7,494 rebaunds",
+        "6,135 assist",
+        "7 All Star",
+      ],
+    }]
