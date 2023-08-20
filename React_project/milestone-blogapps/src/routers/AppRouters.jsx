d8b95be1aa60de2f1bb930bd08d450658a40813f
yelizesgi <yelizesgi2014@gmail.com>
2023-08-20 23:18:41 +0300

    milestone project edit

---
+++ React_project/milestone-blogapps/src/routers/AppRouters.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouters, Routes, Route} from "react-router-dom"
+
+import React from 'react'
+
+const AppRouters = () => {
+  return (
+    <BrowserRouters>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouters>
+  )
+}
+
+export default AppRouters
